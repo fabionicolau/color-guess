2c6faf27dd2d95429e60d985499111519edee9a5
fabionicolau <fabionicolaujr@gmail.com>
2022-01-03 19:14:40 -0300

    requisitos 5 a 7

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const header = document.querySelector('#header');
+const header = document.querySelector('header');
 const ball = document.getElementsByClassName('ball');
+const answer = document.getElementById('answer');
+const button = document.getElementById('reset-game');
+let contador = 0;
 
 function randomColors() {
   const red = Math.round(Math.random() * 255);
@@ -8,17 +11,51 @@ function randomColors() {
   const rgb = `rgb(${red}, ${green}, ${blue})`;
   return rgb;
 }
-function addColorGuess() {
+
+let color = randomColors();
+function stringColorGuess() {
   const paragrafo = document.createElement('p');
-  paragrafo.innerText = randomColors().replace('rgb', '');
+  paragrafo.innerText = color.replace('rgb', '');
   paragrafo.setAttribute('id', 'rgb-color');
   header.appendChild(paragrafo);
 }
-addColorGuess();
+stringColorGuess();
 
 function ballRandomColors() {
   for (let i = 0; i < ball.length; i += 1) {
     ball[i].style.backgroundColor = randomColors();
   }
+  ball[Math.round(Math.random() * 5)].style.backgroundColor = color;
 }
 ballRandomColors();
+
+function score() {
+  const paragrafo = document.createElement('p');
+  paragrafo.innerText = `placar: ${0}`;
+  paragrafo.setAttribute('id', 'score');
+  header.appendChild(paragrafo);
+}
+score();
+
+const placar = document.getElementById('score');
+function answerColor(event) {
+  if (event.target.style.backgroundColor === color) {
+    answer.innerText = 'Acertou!';
+    contador += 3;
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
+  }
+  placar.innerText = `placar: ${contador}`;
+}
+for (let i = 0; i < ball.length; i += 1) {
+  ball[i].addEventListener('click', answerColor);
+}
+
+const rgbColor = document.getElementById('rgb-color');
+function resetGame() {
+  color = randomColors();
+  rgbColor.innerText = color.replace('rgb', '');
+  ballRandomColors();
+  answer.innerText = 'Escolha uma cor';
+}
+button.addEventListener('click', resetGame);
